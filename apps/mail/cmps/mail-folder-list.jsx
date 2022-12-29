856edff01aff6b178c290219e7dc62555897e18e
Darkfall48 <sidney@sebban.be>
2022-12-29 17:26:36 +0200

    Update mail-folder-list.jsx

--- apps/mail/cmps/mail-folder-list.jsx
+++ apps/mail/cmps/mail-folder-list.jsx
@@ -3,5 +3,13 @@
 const { useState, useEffect } = React
 
 export function MailFolderList({ onSetFilter }) {
-  return <section className="mail-folder-list"></section>
+  return (
+    <section className="mail-folder-list">
+      <button>Inbox</button>
+      <button>Starred</button>
+      <button>Sent</button>
+      <button>Drafts</button>
+      <button>Trash</button>
+    </section>
+  )
 }
