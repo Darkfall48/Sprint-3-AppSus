88ff36754599e488b16e5afff4b5fbd0d59cf737
Keren Siebner <kerens969@gmail.com>
2022-12-30 01:07:41 +0200

    styling changes to notes

--- apps/note/cmps/bg-color-select.jsx
+++ apps/note/cmps/bg-color-select.jsx
@@ -3,29 +3,53 @@ const { useState, useEffect, useRef } = React
 export function BgColorSelection({ note, onChangeBackgroundColor }) {
     const [color, setColor] = useState('default')
     const ref = useRef(null)
-    const handleClick = () => {
+    //     const handleClick = (ev) => {
+    // console.log('ev', ev)
+    //         ev.stopImmediatePropagation()
+    //         console.log(ev.traget)
+    //         const chosenColor = ref.current.getAttribute('data-color')
+    //         console.log('chosenColor', chosenColor)
+    //         return chosenColor
+    //         // setColor(chosenColor)
+    //     }
+    function handleClick(ev) {
+        console.log('ev', ev)
+        ev.stopPropagation()
         // ev.stopImmediatePropagation()
         const chosenColor = ref.current.getAttribute('data-color')
         console.log('chosenColor', chosenColor)
-        return chosenColor
-        // setColor(chosenColor)
+        setColor(chosenColor) 
     }
+console.log('color', color)
+    // const handleClick = event => {
+    //     console.log(event)
+    // }
+
+
     return <div className="color-palette-container">
+
+        <button className="color-select default-bg" ref={ref} onClick={handleClick} data-color="default"  ></button>
+        <button className="color-select purple-bg" ref={ref} onClick={handleClick} data-color="purple"  ></button>
+        {/* <div className="color-select default-bg" ref={ref} onClick={() => {
+            onChangeBackgroundColor(note, handleClick())
+        }} data-color="default"  ></div> */}
+        {/* 
         <div className="color-select purple-bg" ref={ref} onClick={() => {
             onChangeBackgroundColor(note, handleClick())
-        }} data-color="purple"  ></div>
-        
+        }} data-color="purple"  ></div> */}
+
         <div className="color-select pink-bg" ref={ref} onClick={() => {
             onChangeBackgroundColor(note, handleClick())
         }} data-color="pink"  ></div>
 
-        <div className="color-select blue-bg" ref={ref} onClick={() => {
-            onChangeBackgroundColor(note, handleClick())
-        }} data-color="blue"  ></div>
-        
         <div className="color-select green-bg" ref={ref} onClick={() => {
             onChangeBackgroundColor(note, handleClick())
         }} data-color="green"  ></div>
+
+        <div className="color-select blue-bg" ref={ref} onClick={() => {
+            onChangeBackgroundColor(note, handleClick())
+        }} data-color="blue"  ></div>
+
 
     </div>
 }
