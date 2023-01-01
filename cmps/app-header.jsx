9af7305227207b7210fb88c771b97eaa635a7c95
Darkfall48 <sidney@sebban.be>
2023-01-01 04:53:20 +0200

    Home Page is now fully responsible

--- cmps/app-header.jsx
+++ cmps/app-header.jsx
@@ -1,20 +1,42 @@
+const { useEffect, useRef } = React
+
 const { Link, NavLink } = ReactRouterDOM
 
 export function AppHeader() {
+  const linksHeaderRef = useRef(null)
+
+  function toggleBar() {
+    if (linksHeaderRef.current.style.display === 'flex') {
+      console.log('Display to none')
+      linksHeaderRef.current.style.display = ''
+    } else {
+      linksHeaderRef.current.style.display = 'flex'
+    }
+  }
+
   return (
     <header className="app-header main-layout full">
       <nav className=" nav-header header-layout">
-        <Link to="/">
-          <div className="fa-solid fa-horse logo-header">
-            <span className="title-header">AppSus</span>
-          </div>
-        </Link>
-        <div className="links-header">
+        <article className="nav-header-logo-section">
+          <Link to="/">
+            <div className="fa-solid fa-horse logo-header">
+              <span className="title-header">AppSus</span>
+            </div>
+          </Link>
+        </article>
+        <article className="nav-header-links-section" ref={linksHeaderRef}>
           <NavLink to="/">Home</NavLink>
           <NavLink to="/note">Note</NavLink>
           <NavLink to="/mail">Mail</NavLink>
           <NavLink to="/book">Book</NavLink>
-        </div>
+        </article>
+        <article className="nav-header-more-section">
+          <a
+            title="Show more options"
+            onClick={toggleBar}
+            className="fa-solid fa-bars"
+          ></a>
+        </article>
       </nav>
     </header>
   )
