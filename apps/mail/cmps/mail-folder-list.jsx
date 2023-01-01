d018fc8e6d10461996b7de40de2fa5aaa8a692b0
Darkfall48 <sidney@sebban.be>
2023-01-01 09:18:20 +0200

    Update mail-folder-list.jsx

--- apps/mail/cmps/mail-folder-list.jsx
+++ apps/mail/cmps/mail-folder-list.jsx
@@ -1,15 +1,36 @@
 // TODO: • Allow filtering by different folders: inbox / sent / trash/ draft
 
 const { useState, useEffect } = React
+import { showErrorMsg } from '../../../services/event-bus.service.js'
 
 export function MailFolderList({ onSetFilter }) {
   return (
     <section className="mail-folder-list-container">
-      <a className="fa-solid fa-inbox" title="Inbox"></a>
-      <a className="fa-solid fa-star" title="Starred Mails"></a>
-      <a className="fa-solid fa-arrow-right" title="Sent Mails"></a>
-      <a className="fa-solid fa-pencil" title="Drafts"></a>
-      <a className="fa-solid fa-trash" title="Trash"></a>
+      <a
+        onClick={() => showErrorMsg('Not implemented yet!:(')}
+        className="fa-solid fa-inbox"
+        title="Inbox"
+      ></a>
+      <a
+        onClick={() => showErrorMsg('Not implemented yet!:(')}
+        className="fa-solid fa-star"
+        title="Important Mails"
+      ></a>
+      <a
+        onClick={() => showErrorMsg('Not implemented yet!:(')}
+        className="fa-solid fa-arrow-right"
+        title="Sent Mails"
+      ></a>
+      <a
+        onClick={() => showErrorMsg('Not implemented yet!:(')}
+        className="fa-solid fa-pencil"
+        title="Drafts"
+      ></a>
+      <a
+        onClick={() => showErrorMsg('Not implemented yet!:(')}
+        className="fa-solid fa-trash"
+        title="Trash"
+      ></a>
     </section>
   )
 }
